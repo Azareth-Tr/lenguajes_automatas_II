80ec72d3f3dbb8156271280b10f432499ad67641
Azareth-Tr <azareth289@gmail.com>
2026-07-01 22:22:09 -0600

    feat: agreamos la lectura de archivos

--- Competencia 2/ejercicio2_v2/Text.py
+++ Competencia 2/ejercicio2_v2/Text.py
@@ -2,10 +2,14 @@
 #Importa ANTLR para funciones 
 from antlr4 import *
 from ExprLexer import ExprLexer
+import sys
 
+# libreria para leer archivos
 #Lo que obtiene es la entrada, analiza el texto y lo separa en tokens
 
-lexer = ExprLexer(InputStream(input("? ")))
+input_stream = FileStream(sys.argv[1])
+
+lexer = ExprLexer(input_stream) # en vez de leer la terminal, lee el archivo que le pasas como argumento al script
 # toma los tokens que produjo el lexer y los guarda en un flujo/lista
 
 tokens = CommonTokenStream(lexer)
